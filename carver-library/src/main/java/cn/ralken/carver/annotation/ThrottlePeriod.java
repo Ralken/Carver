5cbf97c6932212f3aa45dba8610f15e411369821
Ralken Liao <zeroyakon@gmail.com>
2017-12-27 14:54:09 +0800

    New @ThrottlePeriod annotation support that intercepts frequent method invoking.

---
+++ carver-library/src/main/java/cn/ralken/carver/annotation/ThrottlePeriod.java
@@ -0,0 +1,8 @@
+package cn.ralken.carver.annotation;
+
+/**
+ * Created by Ralken Liao on 27/12/2017.
+ */
+
+public @interface ThrottlePeriod {
+}
---
+++ carver-library/src/main/java/cn/ralken/carver/weave/ThrottlePeriodAspect.java
@@ -0,0 +1,8 @@
+package cn.ralken.carver.weave;
+
+/**
+ * Created by Ralken Liao on 27/12/2017.
+ */
+
+public class ThrottlePeriodAspect {
+}
